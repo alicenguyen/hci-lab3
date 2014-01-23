59416f71ca90e94bfab964cab38582e22749881c
Alice Nguyen <a8nguyen@ucsd.edu>
2014-01-23 14:30:19 -0800

    initial and final push

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -2,17 +2,56 @@
 
 // Call this function when the page loads (the "ready" event)
 $(document).ready(function() {
-	initializePage();
+    initializePage();
 })
 
 /*
  * Function that is called when the document is ready.
  */
 function initializePage() {
-	$("#testjs").click(function(e) {
-		$('.jumbotron h1').text("Javascript is connected");
-	});
+    $("#testjs").click(function(e) {
+        $('.jumbotron h1').text("Experience Chan! It's not a lot of questions. Too many questions is the Chan disease. The best way is just to observe the noise of the world. The answer to your questions? Ask your own heart.");
+        $("#testjs").text("Please wait...");
+        $(".jumbotron p").toggleClass("active");
+    });
+    // Add any additional listeners here
+    // example: $("#div-id").click(functionToCall);
+    $("a.thumbnail").click(projectClick);
 
-	// Add any additional listeners here
-	// example: $("#div-id").click(functionToCall);
+    $("#submitBtn").click(updateProject);
 }
+
+function updateProject(e) {
+   var projectID = $('#project').val();
+   $(projectID).animate({
+      width: $('#width').val()
+   });
+
+   var newText = $('#description').val();
+   $(projectID + " .project-description").text(newText);
+}
+
+
+function projectClick(e) {
+    // Cancel the default action, which prevents the page from reloading
+    e.preventDefault();
+
+    // In an event listener, $(this) is the leement that fired the event
+    var projectTitle = $(this).find("p").text();
+    console.log('projectTitle is ' + projectTitle);
+    $('.jumbotron h1').text(projectTitle);
+
+    var containingProject = $(this).closest(".project"); 
+        var description = $(containingProject).find(".project-description");
+    if (description.length == 0) { 
+        $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>"); 
+    } else { 
+        if($(description).is(":visible")) 
+            description.fadeOut();
+        else
+            description.fadeIn();
+    }
+}
+
+
+
